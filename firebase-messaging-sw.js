d116e5bce255452990fc103c9d93299c8b0aaf1d
Jesus3x <lpbs10x@gmail.com>
2025-06-16 01:20:28 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,11 +2,12 @@ importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging.js");
 
 firebase.initializeApp({
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_AUTH_DOMAIN",
-  projectId: "SEU_PROJECT_ID",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyDAYD7DhRovj0PYHUSpOq5YE1rQXIiOPkc".
+  authDomain: "chat-trader.firebaseapp.com",
+  projectId: "chat-trader",
+  messagingSenderId: "722204996123",
+  appId: "1:722204996123:web:6ab7fd6a6b5646c3e0bebe",
+  measurementId: "G-N36TE38K29"
 });
 
 const messaging = firebase.messaging();
